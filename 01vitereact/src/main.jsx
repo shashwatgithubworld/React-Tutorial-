9a609b6a18059cb4eeae0f5bf509adbe4a5eb278
Shashwat Kumar <shashwatsinghraj94@gmail.com>
2024-02-17 01:14:16 +0530

    created custom React using JS

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,7 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+// function MyApp(){
+//     return(
+//         <div>
+//         <h2>Hello from MyApp function</h2>
+//         </div>
+//     )
+// }
+
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google'
+// }
+
+
+// const anotherElement = (
+//     <a href="http:/google.com" target='_blank'>Visit Google</a>
+// )
+
+const anotherUser = 'Shashwat is Back'
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target:'_blank'},
+    'Click me to visit Google',
+    anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
-    <App />
+    reactElement
 )
